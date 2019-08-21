27877ed7fe9fd1aa148772f9dd0a8be1d754add0
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-08-21 11:42:42 +0300

    after release adjustments and new fun 'ihpd_browse'

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# ihpdr 1.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
+
+# ihpdr 1.0.0
+
+* CRAN Release
---
+++ R/browse.R
@@ -0,0 +1,23 @@
+
+#' Quickly browse to important links
+#'
+#' This function take you to webpages associated with the International House
+#' Price Database. Option `app` take you the International Housing Observatory,
+#' while `info` take you to Dallas Fed info page of the data.
+#'
+#' @param wat whether to browse the shiny `app` or the `info` page. Defaults at
+#' `info`.
+#'
+#' @export
+#' @examples
+#' ihpd_browse()
+#' ihpd_browse("info)
+ihpd_browse <- function(wat = c("app", "info")) {
+  wat <- match.arg(wat)
+  ihpd_url <- switch(
+    wat,
+    app = "https://lancs-macro.shinyapps.io/international-housing-observatory/",
+    info =  "https://www.dallasfed.org/institute/houseprice#tab2"
+  )
+  utils::browseURL(ihpd_url)
+}
