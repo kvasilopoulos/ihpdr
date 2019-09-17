4b2be666b48b0ac111350c46a481666a43450aa8
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-09-17 12:19:40 +0100

    added more info in desciption of ihpd_browse

--- R/browse.R
+++ R/browse.R
@@ -2,8 +2,9 @@
 #' Quickly browse to important links
 #'
 #' This function take you to webpages associated with the International House
-#' Price Database. Option `app` take you the International Housing Observatory,
-#' while `info` take you to Dallas Fed info page of the data.
+#' Price Database and retunrs the url invisibly. Option `app` take you the
+#' International Housing Observatory, while `info` take you to Dallas Fed
+#' info page of the data.
 #'
 #' @param wat whether to browse the shiny `app` or the `info` page. Defaults at
 #' `info`.
@@ -13,7 +14,7 @@
 #' @examples
 #'
 #' ihpd_browse()
-ihpd_browse <- function(wat = c("app", "info")) {
+ihpd_browse <- function(wat = c("info", "app")) {
   wat <- match.arg(wat)
   view_url(wat_url(wat))
 }
